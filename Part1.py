2140e55f965882c3961ceed1c2a7bd34a191fc76
Ayush0406 <laddhaayush@gmail.com>
2019-11-14 00:51:26 +0530

    Corrected bug in normal equation solution and improved speed by use of numpy and scikit learn

--- Part1.py
+++ Part1.py
@@ -6,18 +6,21 @@ Created on Tue Oct 22 19:25:45 2019
 """
 
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 f = pd.read_csv("3D_spatial_network.txt", header = None, delimiter = ',')
 f1 = open("cost_values.txt", 'w+')
 
+print(len(f[1]))
 def calc_cost(n, y, y_calc):
-    return (1/(2*n))*sum((y-y_calc)**2)
+    return (1/(2*n))*np.sum((y-y_calc)**2)
 
 
 def gradient_descent(x2, x3, y, learn_rate):
-    b = 1.54
-    c = 0.12
-    d = -0.012
+    b = 4
+    c = -12
+    d = 680
     n = len(y)
     
     y_calc = b*x2 + c*x3 + d
@@ -25,14 +28,15 @@ def gradient_descent(x2, x3, y, learn_rate):
     last_cost = 0
     print("yes")
     i = 0;
-    while abs(last_cost - cost) > 0.0001:
+    while abs(last_cost - cost) > 0.00001:
         i+=1
         y_calc = b*x2 + c*x3 + d
         last_cost = cost
+        print(cost)
         cost = calc_cost(n, y, y_calc)
-        b_deriv = -(1/n)*sum(x2*(y-y_calc))
-        c_deriv = -(1/n)*sum(x3*(y-y_calc))
-        d_deriv = -(1/n)*sum(y-y_calc)
+        b_deriv = -(1/n)*np.sum(x2*(y-y_calc))
+        c_deriv = -(1/n)*np.sum(x3*(y-y_calc))
+        d_deriv = -(1/n)*np.sum(y-y_calc)
         
         b = b - learn_rate * b_deriv
         c = c - learn_rate * c_deriv
@@ -45,7 +49,8 @@ def gradient_descent(x2, x3, y, learn_rate):
     print(b, c, d, '\n')
     f1.write("%d %d %d" %(b, c, d))
         
-gradient_descent(f[1], f[2], f[3], 0.00001)
+f1_train, f1_test, f2_train, f2_test, y_train, y_test = train_test_split(f[1], f[2], f[3], test_size = 0.3, random_state = 0)
+gradient_descent(f1_train, f2_train, y_train, 0.00001)
 f1.close()
         
         
--- Part4.py
+++ Part4.py
@@ -6,6 +6,7 @@ Created on Wed Oct 23 00:10:16 2019
 """
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split 
 
 f = pd.read_csv("3D_spatial_network.txt", header = None, delimiter = ',')
 f1 = open("cost_values.txt", 'w+')
@@ -13,26 +14,48 @@ f1 = open("cost_values.txt", 'w+')
 a = np.zeros([3, 3])
 b = np.zeros([3, 1])
 
+f1_train, f1_test, f2_train, f2_test, y_train, y_test = train_test_split(f[1], f[2], f[3], test_size = 0.3, random_state = 0)
+n = len(f1_train)
+a[0][0] = np.sum(f1_train**2)
+print(a[0][0])
 
-n = len(f[1])
-for i in range(n):
-    """updating values of row1 of 'a' matrix"""
-    a[0][0] += f[1][i]**2
-    a[0][1] += f[1][i]*f[2][i]
-    a[0][2] += f[1][i]
-    
-    a[1][0] += f[1][i]*f[2][i]
-    a[1][1] += f[2][i]**2
-    a[1][2] += f[2][i]
-    
-    a[2][0] += f[1][i]
-    a[2][1] += f[2][i]
-    
-    b[0][0] += f[3][i]*f[1][i]
-    b[1][0] += f[3][i]*f[2][i]
-    b[2][0] += f[3][i]
-    
-a[2][2] = 1
+a[0][1] = np.sum(f1_train*f2_train)
+print(a[0][1])
+
+a[0][2] = np.sum(f1_train)
+print(a[0][2])
+
+a[1][0] = a[0][1]
+
+a[1][1] = np.sum(f2_train*f2_train)
+a[1][2] = np.sum(f2_train)
+a[2][0] = a[0][2]
+a[2][1] = a[1][2]
+a[2][2] = n
+
+b[0][0] = np.dot(np.transpose(y_train), f1_train)
+b[1][0] = np.dot(np.transpose(y_train), f2_train)
+b[2][0] = np.sum(y_train)
+#print(b)
+#n = int(len(f[1])*0.7)
+#for i in range(n):
+#    """updating values of row1 of 'a' matrix"""
+#    a[0][0] += f[1][i]**2
+#    a[0][1] += f[1][i]*f[2][i]
+#    a[0][2] += f[1][i]
+#    
+#    a[1][0] += f[1][i]*f[2][i]
+#    a[1][1] += f[2][i]**2
+#    a[1][2] += f[2][i]
+#    
+#    a[2][0] += f[1][i]
+#    a[2][1] += f[2][i]
+#    
+#    b[0][0] += f[3][i]*f[1][i]
+#    b[1][0] += f[3][i]*f[2][i]
+#    b[2][0] += f[3][i]
+#    
+#a[2][2] = n
 print(a)
 print(b)
 
